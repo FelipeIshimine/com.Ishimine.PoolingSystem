8a0f0be1a6be83185b76c93a9e52eccd328ca4fe
Ishimine <felipecalafate@hotmail.com>
2022-04-12 23:49:12 -0300

    SelfPoolableParticles

--- Runtime/SelfPoolable.cs
+++ Runtime/SelfPoolable.cs
@@ -56,6 +56,8 @@ public class SelfPoolable : MonoBehaviour
     private void OnDestroy()
     {
     }
+    
+    
 }
 
 
---
+++ Runtime/SelfPoolableParticle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class SelfPoolableParticle : SelfPoolable
+{
+    private void Reset()
+    {
+        var main = GetComponent<ParticleSystem>().main;
+        main.stopAction = ParticleSystemStopAction.Callback;
+    }
+
+    private void OnParticleSystemStopped()
+    {
+        Enqueue();
+    }
+}
